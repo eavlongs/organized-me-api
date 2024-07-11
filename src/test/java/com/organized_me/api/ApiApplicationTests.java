f0e8ad035ca4dcbd25d5dcceffc304cd1380f708
eavlongsok <esok@paragoniu.edu.kh>
2024-07-11 13:23:20 +0700

    minor change

---
+++ src/test/java/com/organized_me/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.organized_me.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
